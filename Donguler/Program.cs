a76c1e94c68dab707a73ed1ee18507017abd2a30
Aslidurucan <117647305+Aslidurucan@users.noreply.github.com>
2025-12-28 15:39:30 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array - dizi
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", 
+                "Programlamaya başlangıç için temel kurs", 
+                "Java", "Python" };
+
+
+           for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+           //dizi temelli yapıları tek tek dönmeye yarar foreach 
+           foreach (string kurs in kurslar) 
+            { 
+                Console.WriteLine(kurs); 
+            }
+
+        }
+    }
+}
